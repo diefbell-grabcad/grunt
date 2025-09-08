b6902089f6e7d07452616d7052a313bb28a149f6
Dief Bell <dief.bell@stratasys.com>
2025-09-08 15:08:30 +0100

    Couple of package typings

---
+++ src/grunt-legacy-util.d.ts
@@ -0,0 +1,99 @@
+declare module "grunt-legacy-util" {
+	/**
+	 * Return the "kind" of a value. Like typeof but returns the internal [Class](Class/) value.
+	 * Possible results are "number", "string", "boolean", "function", "regexp", "array", "date",
+	 * "error", "null", "undefined" and the catch-all "object".
+	 */
+	export function kindOf(value: any): string;
+
+	/**
+	 * Return a new Error instance (that can be thrown) with the appropriate message.
+	 * If an Error object is specified instead of message that object will be returned.
+	 * Also, if an Error object is specified for origError and Grunt was run with the --debug 9 option,
+	 * the original Error stack will be dumped.
+	 */
+	export function error(message: string, origError?: Error): Error;
+	export function error(error: Error, origError?: Error): Error;
+	export function error(error: any, origError?: Error): Error;
+
+	/**
+	 * The linefeed character, normalized for the current operating system.
+	 * (\r\n on Windows, \n otherwise)
+	 */
+	export const linefeed: string;
+
+	/**
+	 * Given a string, return a new string with all the linefeeds normalized for the current operating system.
+	 * (\r\n on Windows, \n otherwise)
+	 */
+	export function normalizelf(str: string): string;
+
+	/**
+	 * Recurse through nested objects and arrays, executing callbackFunction for each non-object value.
+	 * If continueFunction returns false, a given object or value will be skipped.
+	 */
+	export function recurse(
+		object: any,
+		callbackFunction: (value: any) => void,
+		continueFunction: (objOrValue: any) => boolean,
+	): void;
+
+	/**
+	 * Return string str repeated n times.
+	 */
+	export function repeat(n: number, str: string): string;
+
+	/**
+	 * Given str of "a/b", If n is 1, return "a" otherwise "b".
+	 * You can specify a custom separator if '/' doesn't work for you.
+	 */
+	export function pluralize(n: number, str: string, separator?: string): string;
+
+	/**
+	 * Spawn a child process, keeping track of its stdout, stderr and exit code.
+	 * The method returns a reference to the spawned child.
+	 * When the child exits, the done function is called.
+	 *
+	 * @param done a function with arguments:
+	 *        error  - If the exit code was non-zero and a fallback wasn't specified,
+	 *                 an Error object, otherwise null.
+	 *        result - The result object is an
+	 *        code   - The numeric exit code.
+	 */
+	export function spawn(
+		options: ISpawnOptions,
+		done: (error: Error, result: ISpawnResult, code: number) => void,
+	): ISpawnedChild;
+
+	/**
+	 * Given an array or array-like object, return an array.
+	 * Great for converting arguments objects into arrays.
+	 */
+	export function toArray<T>(arrayLikeObject: any): T[];
+
+	/**
+	 * Normalizes both "returns a value" and "passes result to a callback" functions to always
+	 * pass a result to the specified callback. If the original function returns a value,
+	 * that value will now be passed to the callback, which is specified as the last argument,
+	 * after all other predefined arguments. If the original function passed a value to a callback,
+	 * it will continue to do so.
+	 */
+	export function callbackify<R>(syncOrAsyncFunction: () => R): (callback: (result: R) => void) => void;
+	export function callbackify<A, R>(syncOrAsyncFunction: (a: A) => R): (a: A, callback: (result: R) => void) => void;
+	export function callbackify<A, B, R>(
+		syncOrAsyncFunction: (a: A, b: B) => R,
+	): (a: A, b: B, callback: (result: R) => void) => void;
+	export function callbackify<A, B, C, R>(
+		syncOrAsyncFunction: (a: A, b: B, c: C) => R,
+	): (a: A, b: B, c: C, callback: (result: R) => void) => void;
+	export function callbackify<A, B, C, D, R>(
+		syncOrAsyncFunction: (a: A, b: B, c: C, d: D) => R,
+	): (a: A, b: B, c: C, d: D, callback: (result: R) => void) => void;
+
+	// Internal libraries
+	export const namespace: any;
+	export const task: typeof import("./util/task");
+
+	// Idk what this is, but it's used in util/task.ts
+	export const _: any;
+}
---
+++ src/minimatch.d.ts
@@ -0,0 +1,76 @@
+/**
+ * {@link https://github.com/isaacs/minimatch}
+ */
+declare module "minimatch" {
+    /**
+     * A minimal matching utility options.
+     *
+     * This is the matching library used internally by npm.
+     * Eventually, it will replace the C binding in node-glob.
+     * It works by converting glob expressions into JavaScript RegExp objects.
+     */
+    interface IMinimatchOptions {
+        /*
+         All options are false by default.
+         */
+
+        /**
+         * Dump a ton of stuff to stderr.
+         */
+        debug?: boolean | undefined;
+
+        /**
+         * Do not expand {a,b} and {1..3} brace sets.
+         */
+        nobrace?: boolean | undefined;
+
+        /**
+         * Disable ** matching against multiple folder names.
+         */
+        noglobstar?: boolean | undefined;
+
+        /**
+         * Allow patterns to match filenames starting with a period,
+         * even if the pattern does not explicitly have a period in that spot.
+         */
+        // Note that by default, a/**\/b will not match a/.d/b, unless dot is set.
+        dot?: boolean | undefined;
+
+        /**
+         * Disable "extglob" style patterns like +(a|b).
+         */
+        noext?: boolean | undefined;
+
+        /**
+         * Perform a case-insensitive match.
+         */
+        nocase?: boolean | undefined;
+
+        /**
+         * When a match is not found by minimatch.match, return a list containing the pattern itself.
+         * When set, an empty list is returned if there are no matches.
+         */
+        nonull?: boolean | undefined;
+
+        /**
+         * If set, then patterns without slashes will be matched against the basename of the path if it contains slashes.
+         * For example, a?b would match the path /xyz/123/acb, but not /xyz/acb/123.
+         */
+        matchBase?: boolean | undefined;
+
+        /**
+         * Suppress the behavior of treating # at the start of a pattern as a comment.
+         */
+        nocomment?: boolean | undefined;
+
+        /**
+         * Suppress the behavior of treating a leading ! character as negation.
+         */
+        nonegate?: boolean | undefined;
+
+        /**
+         * Returns from negate expressions the same as if they were not negated. (Ie, true on a hit, false on a miss.)
+         */
+        flipNegate?: boolean | undefined;
+    }
+}
